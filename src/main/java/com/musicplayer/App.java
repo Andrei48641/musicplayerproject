d8c695817a57e9b67373bb38d67ed9261e3a104a
VasTheBoss <vasiulianalexandru@gmail.com>
2026-04-17 18:23:16 +0300

    vas

--- src/main/java/com/musicplayer/App.java
+++ src/main/java/com/musicplayer/App.java
@@ -4,12 +4,12 @@ import com.musicplayer.backend.DatabaseCleaner;
 import com.musicplayer.backend.MusicScanner;
 import com.musicplayer.frontend.GUI;
 
-public class App { //trolololo
+public class App { //salut
     public static void main(String[] args) {
         System.out.println("connecting to raspbery pi database:");
 
-   //     DatabaseCleaner.clean();
-        MusicScanner.startScan();
+       // DatabaseCleaner.clean();
+        //MusicScanner.startScan();
 
         new GUI();
     }
